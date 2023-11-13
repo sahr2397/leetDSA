6f60880d843c62e2910beccea08f4e180f0d2f7b
Sahana R <bokehlenz@gmail.com>
2023-11-13 20:35:15 +0530

    Time: 161 ms (22.34%) | Memory: 16.5 MB (94.30%) - LeetSync

---
+++ 1386-shift-2d-grid/shift-2d-grid.py
@@ -0,0 +1,28 @@
+class Solution:
+    def shiftGrid(self, grid: List[List[int]], k: int) -> List[List[int]]:
+        '''
+        Map the [row,col] to a 1D array value
+        Calculate the shift for the 1D array
+        Map the 1D index back to [newRow, newCol]
+        '''
+        
+        rows = len(grid)
+        cols = len(grid[0])
+        size = rows*cols
+
+        def posToVal(i,j):
+            return i*cols + j
+        
+        def valToPos(index):
+            return [ index // cols, index % cols ]
+        
+        res = [[0 for i in range(cols)] for j in range(rows)]
+        
+        for i in range(rows):
+            for j in range(cols):
+                newVal = (posToVal(i,j) + k) % size
+                newRow, newCol = valToPos(newVal)
+                res[newRow][newCol] = grid[i][j]
+        
+        return res
+                
