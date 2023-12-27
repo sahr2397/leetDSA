76e29800027ebf82fe509013573d5485397b9419
Sahana R <bokehlenz@gmail.com>
2023-12-27 16:52:16 +0530

    Time: 383 ms (98.87%) | Memory: 22.3 MB (8.93%) - LeetSync

---
+++ 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.py
@@ -0,0 +1,19 @@
+from collections import defaultdict
+class Solution:
+    def equalPairs(self, grid: List[List[int]]) -> int:
+        n=len(grid[0])
+        cg=[]
+        for j in range(n):
+            col=[grid[i][j] for i in range(n)]
+            cg.append(tuple(col))
+
+        hash_map = defaultdict(int)
+        for row in grid:
+            hash_map[tuple(row)] += 1
+
+        tot=0
+        for col in cg:
+            if col in hash_map:
+                tot += hash_map[col]
+
+        return tot
