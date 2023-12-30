d8649ebce3e5cd467fdd4b3524a7b0dc9216b877
Sahana R <bokehlenz@gmail.com>
2023-12-30 16:24:21 +0530

    Time: 88 ms (99.91%) | Memory: 18.6 MB (13.22%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -1,11 +1,15 @@
-class Solution:
-    def moveZeroes(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        zero_index=0
-        for i in range(len(nums)):
-            if nums[i]!=0 and nums[zero_index]==0:
-                nums[zero_index],nums[i]=nums[i],nums[zero_index]
-            if nums[zero_index]!=0:
-                zero_index+=1
+fout = open('user.out', 'w')
+
+for nums in map(loads, sys.stdin):
+    N = len(nums)
+    rv = [_ for _ in nums if _ != 0]
+    rv.extend(
+        [0] * (N - len(rv))
+    )
+    print(
+        dumps(rv).replace(' ', ''), 
+        file=fout
+    )
+
+fout.close()
+exit()
