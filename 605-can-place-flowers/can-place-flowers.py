4f1464081d8c1d5d7de892a28a4944fb45be5c25
Sahana R <bokehlenz@gmail.com>
2024-01-06 03:10:57 +0530

    Time: 145 ms (69.86%) | Memory: 17.6 MB (19.58%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.py
+++ 605-can-place-flowers/can-place-flowers.py
@@ -7,8 +7,8 @@ class Solution:
         if lfb==1 and flowerbed[0]==0: return True
 
         while(end<lfb):
-            win= flowerbed[start:end+1]
-            if sum(win)==0: 
+     
+            if sum(flowerbed[start:end+1])==0: 
                 res+=1
                 start=end
             else: start+=1
@@ -16,8 +16,7 @@ class Solution:
             end=start+2
         
         if start+2==lfb:
-            win = flowerbed[start:]
-            if sum(win)==0: res+=1
+            if sum(flowerbed[start:])==0: res+=1
      
 
         return res>=n
