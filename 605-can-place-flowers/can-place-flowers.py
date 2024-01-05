72c324dd8e0a49e763beec2ace7cd337bed01fa9
Sahana R <bokehlenz@gmail.com>
2024-01-06 02:25:49 +0530

    Time: 148 ms (60.14%) | Memory: 17.7 MB (19.58%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.py
+++ 605-can-place-flowers/can-place-flowers.py
@@ -1,7 +1,7 @@
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
         '''
-        [0,0,0,1,0,1,0,0,0]
+        [0,0,0,1,0,0,1,0,0]end
         [1,0,1,0,1,1,1,0,1,0,1,0]
         '''
         
@@ -10,30 +10,21 @@ class Solution:
         if len(flowerbed)==1 and flowerbed[0]==0: result=1
 
         while end < len(flowerbed):
-            
             window = flowerbed[start:end+1]
-            
-            print(window)
             if sum(window) == 0: 
                 result += 1
                 start=end
             else: start+=1
-                
             end = start + 2
 
        
         if (start+2==len(flowerbed)):
             window = flowerbed[start:]
-            print(window)
             if sum(window) == 0: result += 1
         elif len(flowerbed)>1 and flowerbed[end-4]!=0:
             window = flowerbed[end-3:]
-            print(window)
             if sum(window) == 0: result += 1
         
-
-
-        # print(result)
         return result >= n
             
         
