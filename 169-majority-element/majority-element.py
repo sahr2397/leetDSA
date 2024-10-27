4061b8c0adeca454df9d386f6811dc9003211c48
Sahana R <bokehlenz@gmail.com>
2024-10-27 18:57:58 +0530

    Time: 15 ms (19.78%) | Memory: 18.5 MB (9.11%) - LeetSync

---
+++ 169-majority-element/majority-element.py
@@ -0,0 +1,11 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        
+        n=len(nums)
+        seen={}
+        for num in nums:
+            if num in seen:
+                seen[num]+=1
+            else:
+                seen[num]=1
+            if seen[num]>n//2 : return num
